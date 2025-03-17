092d6f6f456097acc95ecf370a606a9807a09c7b
Octomuski <168337598+Octomuski@users.noreply.github.com>
2025-03-17 11:08:37 +0000

    Create sonarqube.sh

---
+++ sonarqube.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG sudo $USER
+sudo usermod -aG docker $USER
+
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+#sudo cat <<EOF > /etc/systemd/system/docker.service.d/override.conf
+#[Service]
+#ExecStartPost=/bin/chown root:docker /var/run/docker.sock
+#ExecStartPost=/bin/chmod 660 /var/run/docker.sock
+#EOF 
+
+
+sudo tee /etc/systemd/system/docker.service.d/override.conf <<EOF
+[Service]
+ExecStartPost=/bin/chown root:docker /var/run/docker.sock
+ExecStartPost=/bin/chmod 660 /var/run/docker.sock
+EOF
+
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+
+
+
+# To install sonarqube using docker
+
+docker run -d -p 9000:9000 --name sonarqube sonarqube:lts-community
+
+
+
+
